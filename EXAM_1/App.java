ceb443273325676ebaac4c3916b5166a787eab9a
Earl Anthony Gamboa <111488396+Tsukkei@users.noreply.github.com>
2022-09-08 15:00:15 +0800

    Create App.java

---
+++ EXAM_1/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        student Earl = new student("Gamboa", "Earl Anthony", "Z.", "06/11/04", "2022103155", "gamboaez@national-u.edu.ph", true);
+        
+        Earl.sayMyStudentNumber();
+        Earl.sayMystudentEmailAddress();
+        Earl.sayMyamIAwesome();
+    }
+}
